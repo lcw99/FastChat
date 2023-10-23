04a6503a17e9f35f99f790e4f8a1b349c8e36242
lcw99 <changlee99@gmail.com>
2023-10-23 15:52:31 +0900

    llama

--- model_worker.sh
+++ model_worker.sh
@@ -1,2 +1,2 @@
-python -m fastchat.serve.model_worker --model-path $1 --num-gpus $2 --max-gpu-memory 22Gib --model-names polyglot-ko-12.8b-chang-instruct-chat --limit-worker-concurrency 1024 --stream-interval 1 --port $3
+python -m fastchat.serve.model_worker --model-path $1 --num-gpus $2 --max-gpu-memory 22Gib --model-names llama2-ko-chang-instruct-chat --port $3
 
--- openai.sh
+++ openai.sh
@@ -1,2 +1,2 @@
-python3 -m fastchat.serve.openai_api_server --host 127.0.0.1 --port 8888
+python -m fastchat.serve.openai_api_server_lcw --host 127.0.0.1 --port 8888
 
