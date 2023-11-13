1401a6fd17a8151908ffb4daf8aeed9c49b976e4
chang <changlee99@gmail.com>
2023-11-13 12:51:54 +0900

    stop token

--- fastchat/serve/inference.py
+++ fastchat/serve/inference.py
@@ -174,19 +174,20 @@ def generate_stream(
         token = tokens[0]
         output_ids.append(token)
 
-        # lcw
-        ch = tokenizer.decode(
-            [token],
-            skip_special_tokens=False,
-            spaces_between_special_tokens=False,
-            clean_up_tokenization_spaces=True,
-        )
-        print(f"{token}.{ch}.{stop_token_ids}")
-
         if token in stop_token_ids:
             stopped = True
         else:
             stopped = False
+
+        # lcw
+        if stopped:
+            ch = tokenizer.decode(
+                [token],
+                skip_special_tokens=False,
+                spaces_between_special_tokens=False,
+                clean_up_tokenization_spaces=True,
+            )
+            print(f"{token}.{ch}.{stop_token_ids}")
 
         # Yield the output tokens
         if i % stream_interval == 0 or i == max_new_tokens - 1 or stopped:
