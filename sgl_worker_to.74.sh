1a946a75678f4d9c647facb6f9094ffe4a9c3bd3
chang <changlee99@gmail.com>
2024-08-31 09:48:16 +0900

    ㅁㅁ

---
+++ sgl_worker_to.74.sh
@@ -0,0 +1,12 @@
+DEFAULT_PORT=21009
+DEFAULT_NUM_GPUS=2
+DEFAULT_MAX_LEN=8000
+chat_model=$1
+num_gpus="${2:-$DEFAULT_NUM_GPUS}"
+port="${3:-$DEFAULT_PORT}"
+max_model_len="${4:-$DEFAULT_MAX_LEN}"
+
+host=$(hostname -I | awk '{print $1}')
+echo "chat_model=$chat_model"
+echo "host=$host:$port"
+python -m fastchat.serve.sglang_worker --num-gpus $num_gpus --model-names llama2-ko-chang-instruct-chat --model-path $chat_model --controller-address http://14.54.171.144:21001 --worker-address http://$host:$port --limit-worker-concurrency 4 --host $host --port $port --mem-fraction-static 0.7 
