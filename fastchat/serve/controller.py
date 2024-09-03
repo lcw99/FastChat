d2a08c4f5ba1408d851606e264e02d1604625ece
chang <changlee99@gmail.com>
2024-09-03 12:21:47 +0900

    aa

--- fastchat/serve/controller.py
+++ fastchat/serve/controller.py
@@ -206,7 +206,10 @@ class Controller:
             worker_list = ' '.join(f'{q:1.0f}' for q in worker_qlen)
             worker_sum = sum(worker_qlen)
             worker_avg = worker_sum / len(worker_qlen)
-            worker_bar = "█" * round(worker_avg * 2)
+            worker_bar = "▒" * round(worker_avg * 2) 
+            peak = 8
+            min_len = min(peak, len(worker_bar))
+            worker_bar = worker_bar[:min_len] + "█" * (max(0, len(worker_bar)) - peak)
             logger.info(f"queue_lens: {worker_avg:2.1f} {worker_sum:2.0f} [{worker_list}]({min_index}) {worker_bar}")
             return w_name
         else:
