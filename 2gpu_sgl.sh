5c71050f87127ecf520ca1e17a2654d22c6e62a3
lcw99 <changlee99@gmail.com>
2024-09-12 14:37:15 +0900

    aa

---
+++ 2gpu_sgl.sh
@@ -0,0 +1,9 @@
+gpu1=$1
+gpu2=$(expr $gpu1 + 1)
+echo "run on $gpu1,$gpu2"
+CUDA_VISIBLE_DEVICES=$gpu1,$gpu2 bash sgl_worker_to_aws_n_gpu.sh \
+    --port 2101$1 \
+    --num-gpus 1 \
+    --model-attr "-int8" \
+    --mem-fraction-static 0.6 \
+    --model-names "stargio-saju-chat"
