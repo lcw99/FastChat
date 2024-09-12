70d8d512548327931ceec8d2f11138fa2a5842d9
chang <changlee99@gmail.com>
2024-09-12 17:10:58 +0900

    aa

---
+++ 1gpu_sgl_int8.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=$1 bash sgl_worker_to_aws_n_gpu.sh \
+    --port 2101$1 \
+    --num-gpus 1 \
+    --model-attr "-int8" \
+    --mem-fraction-static 0.6 \
+    --model-names "stargio-saju-chat-int8"
