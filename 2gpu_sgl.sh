8ea4c82cc3253d5c20062777c021f1b0fcb4ab3f
chang <changlee99@gmail.com>
2024-09-12 17:14:04 +0900

    aa

--- 2gpu_sgl.sh
+++ 2gpu_sgl.sh
@@ -2,7 +2,7 @@ gpu1=$1
 gpu2=$(expr $gpu1 + 1)
 echo "run on $gpu1,$gpu2"
 CUDA_VISIBLE_DEVICES=$gpu1,$gpu2 bash sgl_worker_to_aws_n_gpu.sh \
-    --port 2101$1 \
+    --port 2101$gpu1 \
     --num-gpus 1 \
     --model-attr "-int8" \
     --mem-fraction-static 0.6 \
