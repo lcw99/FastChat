7b2a45f6e3bbb35fcfa726f61828ecbe23ed6a9a
chang <changlee99@gmail.com>
2024-09-13 10:01:20 +0900

    ㅁㅁ

--- 2gpu_sgl.sh
+++ 2gpu_sgl.sh
@@ -3,7 +3,6 @@ gpu2=$(expr $gpu1 + 1)
 echo "run on $gpu1,$gpu2"
 CUDA_VISIBLE_DEVICES=$gpu1,$gpu2 bash sgl_worker_to_aws_n_gpu.sh \
     --port 2101$gpu1 \
-    --num-gpus 1 \
-    --model-attr "-int8" \
+    --num-gpus 2 \
     --mem-fraction-static 0.6 \
     --model-names "stargio-saju-chat"
