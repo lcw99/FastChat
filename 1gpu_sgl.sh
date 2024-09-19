0ee3e793b3f8a72a9fc95ebd575587a9562d5990
chang <changlee99@gmail.com>
2024-09-20 06:26:05 +0900

    aa

--- 1gpu_sgl.sh
+++ 1gpu_sgl.sh
@@ -1,4 +1,5 @@
 gpu1=0
+mem_fraction_static="0.6"
 while [[ $# -gt 0 ]]; do
     case $1 in
         --gpu)
@@ -7,6 +8,10 @@ while [[ $# -gt 0 ]]; do
             ;;
         --port)
             port="$2"
+            shift 2
+            ;;
+        --mem-fraction-static)
+            mem_fraction_static="$2"
             shift 2
             ;;
         *)
@@ -24,7 +29,7 @@ CUDA_VISIBLE_DEVICES=$gpu1 bash sgl_worker_to_aws_n_gpu.sh \
     --port $port \
     --num-gpus 1 \
     --model-attr "int8" \
-    --mem-fraction-static 0.6 \
+    --mem-fraction-static $mem_fraction_static \
     --model-names "stargio-saju-chat" \
     --sgl-port 300${gpu1}0
 
